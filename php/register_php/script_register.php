7d3baac01cd68acd0f6b697ffda3661dac60fe60
guiaraujoreal <joaoguilhermearaujo1617@gmail.com>
2023-03-13 02:59:16 -0300

    Adição de icones, criação do file de email

--- php/register_php/script_register.php
+++ php/register_php/script_register.php
@@ -29,20 +29,41 @@ $stmt->bind_param("sssssii",$cpf, $nome, $senha,$telefone,$email,$status_user,$e
 
 $stmt->execute();
  
+//Enviar email de confirmação
 
+require_once('PHP_Mail/PHPMailer.php');
+require_once('PHP_Mail/SMTP.php');
+require_once('PHP_Mail/Exception.php');
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+$mail = new PHPMailer(true);
+
+try {
+	$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+	$mail->isSMTP();
+	$mail->Host = 'smtp.gmail.com';
+	$mail->SMTPAuth = true;
+	$mail->Username = 'porqueseraoficial2020@gmail.com';
+	$mail->Password = 'dpvlpwpiqiuiconc';
+	$mail->Port = 587;
+
+	$mail->setFrom('porqueseraoficial2020@gmail.com');
+	$mail->addAddress('joaoguilhermearaujo1617@gmail.com');
+
+	$mail->isHTML(true);
+	$mail->Subject = 'Confirmacao de solicitacao de acesso';
+	$mail->Body = 'Chegou o email teste do <strong>Canal TI</strong>';
+	$mail->AltBody = 'Chegou o email teste do Canal TI';
+
+	if($mail->send()) {
+		header ("location: confirm_email_register.html");
+	} else {
+		echo 'Email nao enviado';
+	}
+} catch (Exception $e) {
+	echo "Erro ao enviar mensagem: {$mail->ErrorInfo}";
+}
 ?>
-
-<html>
-    <head>
-        <link rel="stylesheet" type="text/css" href="style/script_register.css" media="screen" />
-        <title>
-            VacinaCão - Solicitação encaminhada
-        </title>
-        <link rel="icon" type="image/png" href="style/icons/logo.png"/>
-    </head>
-    <body>
-        <div id="cabecalho_register">
-                <img src="style/icons/logo.png" class="logotipo"></div>
-                <img src="style/icons/text_logo.png" class="texto_logo"></div>
-    </body>
-</html>
